e0c1ce2718e40378cf4e0b4dd90b0570bf983a21
Luis Espinal <luisespinal294@gmail.com>
2022-12-17 15:03:06 -0500

    Started the initial portion of my professional portfolio. Installed Tailwind

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className='text-center mt-20 p-10'>
+      <h1 className='text-7xl text-white'>Luis Espinal</h1>
+      <h2 className='text-7xl text-white mt-10'>Web and Swift IOS Development</h2>
+
+      <div className="mt-10">
+        <p className='text-4xl text-slate-300 mt-10'>I'm a Software Developer interested in building projects and solving any problems when they come up. I enjoy working in teams and brainstorming with my teammates in search for the best solution to the problem that we are facing so we can deliver the best user experience possible.</p>
+        <p className='text-4xl text-slate-300 mt-10'>I have a military background which has given me my leadership and fellowship skills. I'm primarily trained in MERN stack but I am very adaptable and very excited to work in collaborative environments.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
