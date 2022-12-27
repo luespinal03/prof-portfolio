8c73accfb02ae8e34e705e33f1055848093b32d5
Luis Espinal <luisespinal294@gmail.com>
2022-12-26 22:58:43 -0400

    Added two more projects with an img, github react icon linked to github repo and began lining some stuff up

--- src/Projects.js
+++ src/Projects.js
@@ -8,22 +8,58 @@ const Projects = () => {
         <div className='text-center mt-10 p-14 bg-slate-100/50'>
             <h2 className='text-7xl text-white'>Projects</h2>
 
-            <div className="flex justify-center mt-40">
-                <div className="rounded-lg shadow-lg bg-white w-[300px] h-[350px]">
-                    <a href="https://github.com/luespinal03/Expense-Tracker-App" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                        <img className="rounded-t-lg" src={images.expenseTracker1} alt="" />
-                    </a>
-                    <div className="p-6">
-                        <h5 className="text-gray-900 text-xl font-medium mb-2">Expense-Tracker</h5>
-                        <p className="text-gray-700 text-base mb-4">
-                            React App
-                        </p>
-                        <a href='https://github.com/luespinal03/Expense-Tracker-App' className=''><AiFillGithub size={35} /></a>
+            <div className='flex flex-wrap mt-10'>
+                <div className="flex justify-center mt-20">
+                    <div className="rounded-lg shadow-lg bg-white w-[300px] h-[350px]">
+                        <a href="https://github.com/luespinal03/Expense-Tracker-App" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                            <img className="rounded-t-lg p-2" src={images.expenseTracker1} alt="" />
+                        </a>
+                        <div className="p-6">
+                            <h5 className="text-gray-900 text-xl font-medium mb-2">Expense-Tracker</h5>
+                            <p className="text-gray-700 text-base mb-4">
+                                React App
+                            </p>
+                            <a href='https://github.com/luespinal03/Expense-Tracker-App' className=''><AiFillGithub size={25} /></a>
+                        </div>
                     </div>
                 </div>
 
+                <div className="flex justify-center mt-20 ml-10">
+                    <div className="rounded-lg shadow-lg bg-white w-[300px] h-[350px]">
+                        <a href="https://github.com/luespinal03/Expense-Tracker-App" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                            <img className="rounded-t-lg p-2" src={images.gameshak1} alt="" />
+                        </a>
+                        <div className="p-6">
+                            <h5 className="text-gray-900 text-xl font-medium mb-2">FullStack Ecomm-App</h5>
+                            <p className="text-gray-700 text-base mb-4">
+                                React App
+                            </p>
+                            <div className='flex flex-wrap'>
+                                <a href='https://github.com/luespinal03/ecomm-app-client-side' className='px-2'><AiFillGithub size={25} /></a>
+                                <a href='https://github.com/luespinal03/ecomm-app-server-side' className='px-2'><AiFillGithub size={25} /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex justify-center mt-20 ml-10">
+                    <div className="rounded-lg shadow-lg bg-white w-[300px] h-[350px]">
+                        <a href="https://github.com/luespinal03/Expense-Tracker-App" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                            <img className="rounded-t-lg p-2" src={images.hangman1} alt="" />
+                        </a>
+                        <div className="p-6">
+                            <h5 className="text-gray-900 text-xl font-medium mb-2">HangMan-Game</h5>
+                            <p className="text-gray-700 text-base mb-4">
+                                React App
+                            </p>
+                            <a href='https://github.com/luespinal03/hangman-game' className='px-2'><AiFillGithub size={25} /></a>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
+
     )
 }
 
